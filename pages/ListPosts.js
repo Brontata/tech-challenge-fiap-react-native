6ef2d1f5d386a8c0a77239ce31a7e7da64dcdd57
Guilherme Henrique Mello e Almeida <guilhermehenriquemelloealmeida@hotmail.com>
2024-12-02 20:49:41 -0300

    Minor fixes

--- pages/ListPosts.js
+++ pages/ListPosts.js
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, View, TextInput, StyleSheet } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 import postsService from '../services/Posts';
 import Post from '../components/Post';
 const ListPosts = () => {
     const [posts, setPosts] = useState([]);
+    const [search, setSearch] = useState('');
     const isFocused = useIsFocused();
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await postsService.getPosts();
+                const response = await postsService.getPosts(search);
                 setPosts(response);
             } catch (error) {
                 console.error('Error fetching posts:', error);
@@ -19,14 +20,55 @@ const ListPosts = () => {
         if (isFocused) {
             fetchPosts();
         }
-    }, [isFocused]);
+    }, [isFocused, search]);
+
+    const handleSearch = (text) => {
+        setSearch(text);
+    };
+
+    const filteredPosts = posts.filter(item => {
+        const title = item.title.toLowerCase();
+        const description = item.description.toLowerCase();
+        const searchLower = search.toLowerCase();
+
+        return (
+            title.includes(searchLower) ||
+            description.includes(searchLower)
+        );
+    });
+
     return (
-        <FlatList
-            data={posts}
-            renderItem={({ item }) => <Post post={item} />}
-        >
-        </FlatList>
+        <View>
+            <View style={styles.searchContainer}>
+                <TextInput
+                    placeholder="Pesquisar por nome ou descrição"
+                    style={styles.searchInput}
+                    value={search}
+                    onChangeText={handleSearch}
+                />
+            </View>
+            <FlatList
+                data={filteredPosts}
+                renderItem={({ item }) => <Post post={item} />}
+            >
+            </FlatList>
+        </View>
+
     )
-}
+};
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        padding: 15,
+    },
+    searchInput: {
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+    },
+});
 
 export default ListPosts;
+
--- pages/PostDetails.js
+++ pages/PostDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PostDetailsView from '../components/PostDetailsView';
+import getToken from '../services/GetToken';
+import { getRoleFromToken } from '../utils/tokenUtils';
+import { useAuth } from '../Hooks/useAuth';
 
 const PostDetails = ({ route, navigation }) => {
     const { post } = route.params;
-
+    const { role } = useAuth();
+    const isAdmin = role == "PROFESSOR"
+    
     return (
-        <PostDetailsView navigation={navigation} route={route} post={post} admin={true} />
+        <PostDetailsView navigation={navigation} route={route} post={post} admin={isAdmin} />
     );
 }
 
